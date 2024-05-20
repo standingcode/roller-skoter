5ff89d24cc69d6bf7b75449d283c948f99716e70
Paul Standing <paultobiasstanding@gmail.com>
2024-05-20 12:02:31 +0300

    Shop has sliding doors, starting to get control of layers so the player could go into the shop.

---
+++ Assets/ShopDoor.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class ShopDoor : MonoBehaviour
+{
+	[SerializeField]
+	private Animator animator;
+
+	[ContextMenu("OpenDoors")]
+	public void OpenDoors()
+	{
+		animator.SetBool("DoorIsOpen", true);
+	}
+
+	[ContextMenu("CloseDoors")]
+	public void CloseDoors()
+	{
+		animator.SetBool("DoorIsOpen", false);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		OpenDoors();
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		CloseDoors();
+	}
+}
--- Assets/_scripts/Player/PlayerManager.cs
+++ Assets/_scripts/Player/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -9,6 +10,17 @@ public class PlayerManager : MonoBehaviour
 
 	[SerializeField]
 	private Rigidbody2D mainRigidbody;
+
+	[SerializeField]
+	private Transform playerScalableRoot;
+
+	[SerializeField]
+	private SortingGroup sortingGroup;
+
+	[SerializeField]
+	private float playerBackgroundScale = 0.4f;
+
+	private Vector3 playerOriginalScale;
 
 	private Vector2 startPosition;
 	private Quaternion startRotation;
@@ -26,5 +38,20 @@ public class PlayerManager : MonoBehaviour
 		playerControl.ZeroAllForcesAndSpeed();
 		mainRigidbody.transform.position = startPosition;
 		mainRigidbody.transform.rotation = startRotation;
+	}
+
+	[ContextMenu("MovePlayerToBackgroundLayer")]
+	public void MovePlayerToBackgroundLayer()
+	{
+		playerOriginalScale = playerScalableRoot.localScale;
+		sortingGroup.sortingLayerID = SortingLayer.NameToID("PlayerBackgroundLayer");
+		playerScalableRoot.localScale = new Vector3(playerBackgroundScale, playerBackgroundScale, 1);
+	}
+
+	[ContextMenu("MovePlayerToForegroundLayer")]
+	public void MovePlayerToForegroundLayer()
+	{
+		sortingGroup.sortingLayerID = SortingLayer.NameToID("Player");
+		playerScalableRoot.localScale = playerOriginalScale;
 	}
 }
